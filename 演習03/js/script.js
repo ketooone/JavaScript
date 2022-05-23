4a7974891efcd0c2395b1045cebee76cbcef13d1
ketooone <ketone.1014@gmail.com>
2022-05-23 11:45:02 +0900

    変数と演算子の利用

---
+++ 演習03/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let num1 = 100;
+let num2 = 20;
+let arr1 = [10, 20, 30, 40];
+console.log("num1 / num2の結果");
+console.log(num1 / num2);
+console.log("配列の要素数を表示");
+console.log(arr1.length);
+console.log("配列の３番目の値が25より大きいかどうか");
+console.log(arr1[2] > 25);
