7ee439e90589f9d53d52a80b64688c43ef3a3863
ketooone <ketone.1014@gmail.com>
2022-05-24 13:14:28 +0900

    演習12forrmの要素を取得し表示

--- 演習12/js/script.js
+++ 演習12/js/script.js
@@ -1,21 +1,59 @@
 "use strict";
 
-function getDataById(id){
+function getCustomerDataById(id){
     const elm = document.getElementById(id);
-    return elm.innerText;
+    return elm.value;
 }
 
-function getDataByName(name){
+function getColorDataByName(name){
     const elm = document.getElementsByName(name);
-    return elm.innerText;
+    for (let i = 0; i < elm.length; i++){
+        if (elm[i].checked){
+            return elm[i].value;
+        }
+    }
+    return null;
 }
 
-
-//todo 複数選択を取得する際に改行なしでconsoleに出力する
-function printMultipleDataById(id){
+function getBloodtypeDataById(id){
     const elm = document.getElementById(id).options;
-
     for (let i = 0; i < elm.length; i++){
-        // console.log(elm.)
+        if (elm[i].selected){
+            return elm[i].innerText;
+        }
     }
 }
+
+//todo 複数選択を取得する際に改行なしでconsoleに出力する
+function getMultipleDataById(id){
+    const elm = document.getElementById(id).options;
+    let selects = [elm.length]; //selectされた要素だけを入れる関数
+    let j = 0;
+
+    for (let i = 0; i < elm.length; i++){
+        if (elm[i].selected){
+            selects[j] = elm[i].innerText;
+            j++;
+        }
+    }
+    return selects;
+}
+
+function setMultipleDataToText(data){
+    let text = "";
+
+    for (let i = 0; i < data.length; i++){
+        text += data[i];
+        if (i != data.length - 1){
+            text += "と";
+        }
+    }
+    return text;
+}
+
+console.log(getCustomerDataById("customer") + "さん、こんにちは！");
+console.log(getColorDataByName("color") + "が選択されています");
+console.log(getBloodtypeDataById("bloodtype") + "が選択されています");
+let data = getMultipleDataById("hobby"); //趣味のデータを配列で取得
+let text = setMultipleDataToText(data); //配列をテキストに変換
+console.log(text + "が選択されています");
