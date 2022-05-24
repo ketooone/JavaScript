508c3167f79aa94d8c92a979e82d25fd070881f8
ketooone <ketone.1014@gmail.com>
2022-05-24 16:45:48 +0900

    興味なしの文言を追加

--- 演習14/JS/script.js
+++ 演習14/JS/script.js
@@ -3,10 +3,12 @@
 window.onload = function(){
     document.getElementById("clickbutton").onclick = function(){
         let elm = document.getElementsByName("interest");
+        let table = document.getElementById("table");
+        let syainname = table.rows[0].cells[1].innerText;
         if (elm[0].checked){
-            let table = document.getElementById("table");
-            let syainname = table.rows[0].cells[1].innerText;
             alert(syainname + "さん、回答ありがとうございます！ぜひ一緒に山登りしましょう！");
+        } else if (elm[1].checked){
+            alert(syainname + "さん、回答ありがとうございます！残念です、他にも興味があれば教えてください！");
         }
     };
 };
