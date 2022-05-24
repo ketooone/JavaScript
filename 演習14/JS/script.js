e589e4c0ab15a54f73dc9bdb30f1fb82a99a593c
ketooone <ketone.1014@gmail.com>
2022-05-24 16:50:31 +0900

    アンケート欄の名前をalertに反映

--- 演習14/JS/script.js
+++ 演習14/JS/script.js
@@ -3,12 +3,11 @@
 window.onload = function(){
     document.getElementById("clickbutton").onclick = function(){
         let elm = document.getElementsByName("interest");
-        let table = document.getElementById("table");
-        let syainname = table.rows[0].cells[1].innerText;
+        let customer = document.getElementsByName("customer");
         if (elm[0].checked){
-            alert(syainname + "さん、回答ありがとうございます！ぜひ一緒に山登りしましょう！");
+            alert(customer[0].value + "さん、回答ありがとうございます！ぜひ一緒に山登りしましょう！");
         } else if (elm[1].checked){
-            alert(syainname + "さん、回答ありがとうございます！残念です、他にも興味があれば教えてください！");
+            alert(customer[0].value + "さん、回答ありがとうございます！残念です、他にも興味があれば教えてください！");
         }
     };
 };
